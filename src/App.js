2ad1db3b370ccefbf81fa557cf9b73cc5845593b
sjkyomg <sjkyomg@gmail.com>
2023-09-13 09:33:11 +0900

    9월 13일 (수) 따봉,모달 수정 ,글제목 추가

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ function App() {
 
   let post = '테스트 글제목';
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬 독학','자바 독학']);
-  let [따봉,따봉변경] =useState(0);
+  let [따봉,따봉변경] = useState([0,0,0,0]);
   let [modal,setModal] = useState(false);
+  let [title,setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
   [1,2,3].map(function(a){
     console.log(a);
@@ -19,7 +21,7 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      <button onClick={()=>{
+      {/* <button onClick={()=>{
         let copy =[...글제목];
         copy.sort();
         글제목변경(copy);
@@ -45,25 +47,35 @@ function App() {
       <div className='list'>
         <h4 onClick={() => setModal(!modal)}>{post}<span>👍</span>0</h4>
         <p>9월 9일 발행</p>
-      </div>
+      </div> */}
 
 
       {
         글제목.map(function(a,i){
           return(
             <div className='list' key={i}>
-              <h4 onClick={() => setModal(!modal)}>{글제목[i]}
-              </h4>
+              <h4 onClick={() => {setModal(!modal); setTitle(i)}}>{글제목[i]}
+              <span onClick={(e)=>{
+                let copy = [...따봉];
+                copy[i] = copy[i] +1;
+                따봉변경(copy);
+                }}>👍</span> {따봉[i]}</h4>
               <p>9월 11일 발행</p>
           </div>
           )
         })
       }
 
-
+      
+      <input onChange ={(e) => {입력값변경(e.target.value)}} ></input>
+      <button onClick ={() =>{
+        let copy = [...글제목];
+        copy = copy;
+        글제목변경(copy); 
+      }}></button>
 
       {
-        modal == true ? <Modal/> : null
+        modal == true ? <Modal title={title} color={'yellow'} 글제목변경={글제목변경} 글제목={글제목}/> : null
       }
     
 
--- src/Modal.js
+++ src/Modal.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
 
-function Modal() {
+
+
+function Modal(props) {
+    let [title,setTitle] = useState(0);
+
         return(
-        <div className='modal'>
-            <h4>제목</h4> 
+        <div className='modal' style={{background : props.color}}>
+            <h4>{props.글제목[props.title]}</h4> 
             <p>날짜</p>
             <p>상세내용</p>
+            <button onClick={()=> props.글제목변경(['여자코트 추천','강남우동 맛집'])}>글 수정</button>
         </div>
         )
 }
